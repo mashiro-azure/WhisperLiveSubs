05b54f41f148549fded4e75032018af2017cbe18
mashiro-azure <kinglokhenry@gmail.com>
2023-08-17 23:01:21 +0800

    disable flask reloader

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000", debug=True)
+    app.run(host="127.0.0.1", port="5000", debug=True, use_reloader=False)
